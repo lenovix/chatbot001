cdf4db9f43ff2fb4cf6c3b7fb182964e9f7d9cfa
lenovix <ichkmlsdr@gmail.com>
2023-06-13 11:44:23 +0700

    first commit

--- app/src/main/java/com/kamilsudarmi/chatbot001/chatbot/ChatbotActivity.kt
+++ app/src/main/java/com/kamilsudarmi/chatbot001/chatbot/ChatbotActivity.kt
@@ -1,10 +1,20 @@
 package com.kamilsudarmi.chatbot001.chatbot
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.kamilsudarmi.chatbot001.api.naiveBayer.ApiClientNB.apiServiceNB
 import com.kamilsudarmi.chatbot001.api.naiveBayer.ChatResponse
 import retrofit2.Call
@@ -12,6 +22,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import com.google.gson.Gson
 import com.kamilsudarmi.chatbot001.Constant
+import com.kamilsudarmi.chatbot001.MainActivity
+import com.kamilsudarmi.chatbot001.R
 import com.kamilsudarmi.chatbot001.api.ApiService
 import com.kamilsudarmi.chatbot001.api.naiveBayer.UserInput
 import com.kamilsudarmi.chatbot001.databinding.ActivityChatbotBinding
@@ -26,11 +38,15 @@ class ChatbotActivity : AppCompatActivity() {
 
     val TAG = "testing"
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatbotBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        permissionCheck()
 
         binding.buttonSend.setOnClickListener {
             val userInput = binding.editTextUserInput.text.toString().trim()
@@ -62,11 +78,14 @@ class ChatbotActivity : AppCompatActivity() {
     }
 
     var unitEmergency: String = ""
+    var addressUser: String = "address"
+    var latLongUser: String = "address"
     private fun analyzeEmergencyInfo(userInput: String) {
         val gson = Gson()
         val requestMap = mapOf("texts" to listOf(userInput))
         val requestBody = RequestBody.create(okhttp3.MediaType.parse("application/json"), gson.toJson(requestMap))
 
+        getLastKnownLocation()
         val call = apiServiceNB.analyzeEmergencyInfo(requestBody)
 
         call.enqueue(object : Callback<List<ChatResponse>> {
@@ -138,5 +157,69 @@ class ChatbotActivity : AppCompatActivity() {
     }
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun permissionCheck() {
+        // Meminta izin jika belum diberikan
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                ChatbotActivity.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        } else {
+            if (checkLocationPermission()) {
+                getLastKnownLocation()
+            } else {
+                // Jika izin tidak diberikan, minta izin
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    ChatbotActivity.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+                )
+            }
+        }
+    }
+    private fun checkLocationPermission(): Boolean {
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        val granted = PackageManager.PERMISSION_GRANTED
+        return ContextCompat.checkSelfPermission(this, permission) == granted
+    }
+
+    private fun getLastKnownLocation() {
+        val locationTextView = findViewById<TextView>(R.id.locationTextView)
+        if (checkLocationPermission()) {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+
+                        val geocoder = Geocoder(this)
+                        val addresses: List<Address> =
+                            geocoder.getFromLocation(latitude, longitude, 1) as List<Address>
+
+                        val address = addresses[0].getAddressLine(0)
+                        val city = addresses[0].locality
+                        val country = addresses[0].countryName
+
+                        locationTextView.text = "$latitude, $longitude\n$address, $city, $country"
+                        addressUser = "$address, $city, $country"
+                        latLongUser = "$latitude, $longitude"
+                    }
+                }
+        } else {
+            // Jika izin tidak diberikan, minta izin
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                ChatbotActivity.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
 }
